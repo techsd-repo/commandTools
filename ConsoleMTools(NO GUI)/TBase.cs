c7c87abc73bab15dd0a7d5f7e1cbef72875d0728
Techsd <techsd>
2015-03-14 11:16:45 -0700

    main sync

---
+++ ConsoleMTools(NO GUI)/TBase.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Diagnostics;
+
+namespace ConsoleMTools_NO_GUI_
+{
+     
+    public static class TBase
+    {
+        public static bool disableMon = false;
+        public static string choice;
+        public static bool runSysMonWithAlerts; // Will impl. this later, not our focus right now
+
+        public static string sysmonCounterDataSet;
+
+
+        static void Main(string[] args)
+        {
+            openMain();
+            
+            if (choice == "1")
+            {
+                //Confirmation
+                Console.WriteLine("You chose to run sysmon with alerts. Are you sure? (y/n)");
+                string c1con = Console.ReadLine();
+                //Get number of counters here
+                Console.WriteLine("How many counters would you like to use (CPU AND MEM only supp. right now)\n");
+                string c1Xcnum = Console.ReadLine();
+                Thread.Sleep(100);
+
+                Console.WriteLine("Please give the counter dataset sep. by commas");
+                sysmonCounterDataSet = Console.ReadLine();
+
+            }
+
+            if (choice == "3")
+            {
+                Console.WriteLine("What process would you like to run(exe file)");
+                string processOpen = Console.ReadLine();
+                Console.WriteLine("Running " + processOpen + "...");
+                Thread.Sleep(200);
+                runProcessWithoutURLOpener(processOpen);
+                openMain();
+            }
+        }
+        public static void openMain() {
+            //Opening
+            Console.WriteLine("Welcome to MTools v1.0 Beta CMD edition (GUI is coming later)\n");
+            Thread.Sleep(200);
+
+            //Options for Tools
+            Console.WriteLine("Choose what tool you need:\n 1) Run sysmon(with alerts)\n 2) Run sysmon (without alerts NOT AVAIL.)\n 3) Run a process\n 4) Run a browser witha URL\n 5) Run a strip down browser(with IE render)\n");
+            choice = Console.ReadLine();
+        }
+        
+        //Function to create a process and run sysmon
+        public static void runSysMonProcess(string arg = null) //For alerts (impl later)
+        {
+            Process sysMonAProcess = new Process();
+            sysMonAProcess.StartInfo.FileName = "MTools + Jarvis.exe";
+
+        }
+
+
+        //Function for running a process without a URL
+        public static void runProcessWithoutURLOpener(string fileName)
+        {
+            Process processWithoutURL = new Process();
+            processWithoutURL.StartInfo.FileName = fileName;
+            processWithoutURL.Start();
+            
+        }
+    
+    } // class
+} // namespace
---
+++ MTools + Jarvis/Program.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Threading;
+using System.Speech.Synthesis;
+using ConsoleMTools_NO_GUI_;
+using System.Text.RegularExpressions;
+
+
+
+
+namespace MTools___Jarvis
+{
+    public class Program
+    {
+
+        private static SpeechSynthesizer synth = new SpeechSynthesizer();
+        
+        //Where all the magic happens
+        static void Main(string[] args)
+        {
+            if (TBase.disableMon == false)
+            {
+                #region perfc
+                //m-cpu counter
+                PerformanceCounter perfCpuCount = new PerformanceCounter("Processor Information", "% Processor Time", "_Total");
+                perfCpuCount.NextValue();
+                //m-mem counter
+                PerformanceCounter perfMemoryCount = new PerformanceCounter("Memory", "Available MBytes");
+                perfMemoryCount.NextValue();
+                //m-uptime counter
+                PerformanceCounter perfUptimeCount = new PerformanceCounter("System", "System Up Time");
+                perfUptimeCount.NextValue();
+                #endregion
+
+               
+
+
+                SpeakAPI("Welcome to Jarvis one point oh!", VoiceGender.Male);
+
+                TimeSpan uptimeSpan = TimeSpan.FromSeconds(perfUptimeCount.NextValue());
+                string vocalUptime = String.Format("The current system uptime is {0} days {1} hours {2} minutes and {3} seconds",
+                    (int)uptimeSpan.TotalDays,
+                    (int)uptimeSpan.Hours,
+                    (int)uptimeSpan.Minutes,
+                    (int)uptimeSpan.Seconds);
+                SpeakAPI(vocalUptime, VoiceGender.Male);
+
+                while (true)
+                {
+                    float currCpuPer = perfCpuCount.NextValue();
+                    float currAvaMem = perfMemoryCount.NextValue();
+
+                    //Searching the TBase.sysmonCounterDataSet for the counters
+                    bool sysmonData1 = Regex.IsMatch(TBase.sysmonCounterDataSet, "\bcpu\b");
+                    bool sysmonData2 = Regex.IsMatch(TBase.sysmonCounterDataSet, "\bmem\b");
+
+                    if (sysmonData1 == true)
+                    {
+                        //CPU update code here
+                    }
+
+                    if (sysmonData2 == true)
+                    {
+                        //Memory update code here
+                    }
+
+                    Console.WriteLine("CPU Load {0}%", currCpuPer);
+                    if (currCpuPer > 70)
+                    {
+                        //CPU 100
+                        if (currCpuPer == 100)
+                        {
+                            //Speak 100
+                            string cpuLoadVocal = String.Format("WARNING: Your CPU is about to catch fire!!");
+                            SpeakAPI(cpuLoadVocal, VoiceGender.Male, 3);
+                        }
+                        else
+                        {
+                            //Else: if cpuLoad > 70 speak current
+                            string cpuLoadVocal = String.Format("The current CPU load is {0}%", (int)currCpuPer);
+                            SpeakAPI(cpuLoadVocal, VoiceGender.Male, 2);
+                        }
+
+                        //Memoy Handle
+                        Console.WriteLine("Avail. Mem: {0}MB", currAvaMem);
+                        Thread.Sleep(1000);
+                        //Logic
+                        if (currCpuPer > 70)
+                        {
+                            if (currCpuPer == 100)
+                            {
+                                string cpuLoadVocal = String.Format("WARNING: Your CPU is about to catch fire!!");
+                                SpeakAPI(cpuLoadVocal, VoiceGender.Male, 3);
+                            }
+                            else
+                            {
+                                string cpuLoadVocal = String.Format("The current CPU load is {0}%", (int)currCpuPer);
+                                SpeakAPI(cpuLoadVocal, VoiceGender.Male, 2);
+                            }
+                        }
+                        if (currAvaMem < 850)
+                        {
+                            string memAvaVocal = String.Format("You have {0}Megabytes of memory ", (int)currAvaMem);
+                            SpeakAPI(memAvaVocal, VoiceGender.Male, 2);
+                        }
+
+                    }
+                    Thread.Sleep(1000);
+
+                }//end of loop
+            }
+        }
+        #region functions
+        //Speak function(API)
+        public static void SpeakAPI(string message, VoiceGender voiceGender)
+        {
+            synth.SelectVoiceByHints(voiceGender);
+            synth.Speak(message);
+        }
+        //Override (so if you don't want to change the rate, you have that option)
+        public static void SpeakAPI(string message, VoiceGender voiceGender, int rate)
+        {
+            synth.Rate = rate;
+            SpeakAPI(message, voiceGender);
+        }
+
+        public static void OpenProcess(string exe)
+        {
+            Process p1 = new Process();
+            p1.StartInfo.FileName = exe;
+
+        }
+
+
+    }
+}
+        #endregion
