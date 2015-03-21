1c74a7f2984cd0b5d44ad3d580d7afc70e32de63
TS2 <ts2email>
2015-03-21 09:31:35 -0400

    fixed functions

--- MTools + Jarvis/Startup.cs
+++ MTools + Jarvis/Startup.cs
@@ -7,13 +7,13 @@ using System.Threading;
 
 namespace MTools___Jarvis
 {
-    class Startup
+    public static class Startup
     {
         public static string c1Xcnum;
         public static string dataset;
-        static void Main(string[] args)
+       
+        public static void runStartup()
         {
-            
             Console.WriteLine("How many counters would you like to use (CPU AND MEM only supp. right now)\n");
             c1Xcnum = Console.ReadLine();
             Thread.Sleep(100);
@@ -21,4 +21,6 @@ namespace MTools___Jarvis
             dataset = Console.ReadLine();
         }
     }
+
+   
 }
