9f1e1de8b18da00d82bf5e4978b3cb4322fedd87
TS2 <ts2email>
2015-03-21 09:26:24 -0400

    Added public vars

--- MTools + Jarvis/Startup.cs
+++ MTools + Jarvis/Startup.cs
@@ -9,13 +9,14 @@ namespace MTools___Jarvis
 {
     class Startup
     {
+        public static string c1Xcnum;
+        public static string dataset;
         static void Main(string[] args)
         {
-            string dataset;
+            
             Console.WriteLine("How many counters would you like to use (CPU AND MEM only supp. right now)\n");
-            string c1Xcnum = Console.ReadLine();
+            c1Xcnum = Console.ReadLine();
             Thread.Sleep(100);
-
             Console.WriteLine("Please give the counter dataset sep. by commas");
             dataset = Console.ReadLine();
         }
