d6b1d76c8627f937564ab8de59e5df73c23c43ab
TS2 <ts2email>
2015-03-21 09:23:32 -0400

    Added startup code, need to change vars

--- MTools + Jarvis/Startup.cs
+++ MTools + Jarvis/Startup.cs
@@ -3,10 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
 
 namespace MTools___Jarvis
 {
     class Startup
     {
+        static void Main(string[] args)
+        {
+            string dataset;
+            Console.WriteLine("How many counters would you like to use (CPU AND MEM only supp. right now)\n");
+            string c1Xcnum = Console.ReadLine();
+            Thread.Sleep(100);
+
+            Console.WriteLine("Please give the counter dataset sep. by commas");
+            dataset = Console.ReadLine();
+        }
     }
 }
